b0943f9beef20bf962cbad3db1364e5e35fd07ee
muhdnaimmuhd61-hub <muhdnaimmuhd61@gmail.com>
2025-11-09 09:56:34 +0100

    app:py
    Reorganize import statements for better readability.

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template_string, request, send_file
-import sqlite3, os, csv, random from werkzeug.utils import secure_filename
 
+import sqlite3
+import os
+import csv
+import random
+from werkzeug.utils import secure_filename
 app = Flask(name) app.config['UPLOAD_FOLDER'] = 'static/photos' os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 
 === Initialize database with 36 States + LGAs ===
